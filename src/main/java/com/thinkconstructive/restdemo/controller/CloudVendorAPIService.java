1e8d9c9d267c007ca6447151e3d11305bf5b100e
umathileepan <thileepan_uma@yahoo.co.uk>
2023-04-14 13:08:50 +0100

    Initial commit of REST API Practice.

---
+++ src/main/java/com/thinkconstructive/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,34 @@
+package com.thinkconstructive.restdemo.controller;
+
+import com.thinkconstructive.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        return cloudVendor;
+                //("C1","Uma","Address 1","123123123");
+
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
